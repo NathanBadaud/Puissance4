54ca971e7945145f6de85594e3872b173017a6a3
Boutin Matthieu <mattdu49@gmail.com>
2015-05-06 10:37:14 +0200

    Update source code.

--- CODE_SOURCE/Puissance_Engine_4/src/com/example/Puissance_Engine_4/MyActivity.java
+++ CODE_SOURCE/Puissance_Engine_4/src/com/example/Puissance_Engine_4/MyActivity.java
@@ -1,15 +1,39 @@
 package com.example.Puissance_Engine_4;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    Button ButtonTitle;
+    Button ButtonMulti;
+    Button ButtonIA;
+    Button ButtonSettings;
+    Activity SettingsMenuActivity;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ButtonTitle = (Button) findViewById(R.id.Title);
+        ButtonMulti = (Button) findViewById(R.id.MultiButton);
+        ButtonIA = (Button) findViewById(R.id.IAButton);
+        ButtonSettings = (Button) findViewById(R.id.settings);
+
+        ButtonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyActivity.this, SettingsMenu.class);
+                startActivity(intent);
+            };
+
+
+        });
     }
 }
