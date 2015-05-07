31530ba88d07b742a5bd6b7471febbfd07dad9a9
BenjaminTessier <benjamin.tessier@epsi.fr>
2015-05-07 15:14:40 +0200

    implémentation des noms de joueurs dans IA game

--- CODE_SOURCE/Puissance_Engine_4/src/com/example/Puissance_Engine_4/Activity_IA.java
+++ CODE_SOURCE/Puissance_Engine_4/src/com/example/Puissance_Engine_4/Activity_IA.java
@@ -2,15 +2,21 @@ package com.example.Puissance_Engine_4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by matthieu on 06/05/2015.
  */
 public class Activity_IA extends Activity {
+    TextView nomJoueur1;
+    TextView nomJoueur2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ia_game_display);
+
+        nomJoueur1 = (TextView) findViewById(R.id.nomJoueur1);
+        nomJoueur2 = (TextView) findViewById(R.id.nomJoueur2);
     }
 }
