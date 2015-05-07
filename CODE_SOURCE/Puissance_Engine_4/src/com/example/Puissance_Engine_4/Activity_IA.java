05622c4108144a100fd5dd0dc03ff2f536b71d2a
BenjaminTessier <benjamin.tessier@epsi.fr>
2015-05-07 11:23:20 +0200

    test gridview

--- CODE_SOURCE/Puissance_Engine_4/src/com/example/Puissance_Engine_4/Activity_IA.java
+++ CODE_SOURCE/Puissance_Engine_4/src/com/example/Puissance_Engine_4/Activity_IA.java
@@ -2,6 +2,10 @@ package com.example.Puissance_Engine_4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by matthieu on 06/05/2015.
@@ -11,5 +15,16 @@ public class Activity_IA extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ia_game_display);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Activity_IA.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
